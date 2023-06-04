6b1810c37b0db3dcd9cbbfb2b0baddbccf7da4f7
ChayFadida <chayfadida1997@gmail.com>
2023-06-04 20:00:06 +0300

    middle

--- src/routes/graph.js
+++ src/routes/graph.js
@@ -1,32 +1,25 @@
 const request = require('request');
 
-
-module.exports = (req, res, connection) => {
+module.exports = (symbol, updateChart) => {
   // Replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
   const apiKey = process.env.API_KEY;
-  const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${apiKey}`;
+  const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=${apiKey}`;
 
   request.get({
     url: url,
     json: true,
     headers: { 'User-Agent': 'request' }
-  }, (err, res, data) => {
+  }, (err, response, data) => {
     if (err) {
       console.log('Error:', err);
-    } else if (res.statusCode !== 200) {
-      console.log('Status:', res.statusCode);
-      console.log(res.body);
-      const openValues = [];
-      for (const date in timeSeries) {
-        if (timeSeries.hasOwnProperty(date)) {
-          const entry = timeSeries[date];
-          const open = parseFloat(entry['1. open']);
-          openValues.push(open);
-        }
-      };
+    } else if (response.statusCode !== 200) {
+      console.log('Status:', response.statusCode);
+      console.log(response.body);
     } else {
       // Data is successfully parsed as a JSON object
       console.log(data);
+      // Pass the retrieved data to the updateChart function
+      updateChart(data);
     }
   });
-}
+};
