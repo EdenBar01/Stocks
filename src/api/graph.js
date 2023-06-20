4fedbecbf894a756726c181505c96a367b7a7a12
ChayFadida <chayfadida1997@gmail.com>
2023-06-20 21:04:58 +0300

    seach is working

--- src/api/graph.js
+++ src/api/graph.js
@@ -35,4 +35,33 @@ router.post('/stocks', (req, res) => {
       }});
 });
 
+router.get('/stocks', (req, res) => {
+  const { stockSymbol} = req.body;
+  const apiKey = process.env.API_KEY;
+  const apiFunction = "SYMBOL_SEARCH"
+  let url = `https://www.alphavantage.co/query?function=${apiFunction}&keywords=${stockSymbol}&apikey={apiKey}`;
+  console.log(url);
+  request.get({
+    url: url,
+    json: true,
+    headers: { 'User-Agent': 'request' }
+  }, (err, response, data) => {
+    if (err) {
+      console.log('Error:', err);
+      res.json({ success: false, error: err });
+    } else if (response.statusCode !== 200) {
+      console.log('Status:', response.statusCode);
+      res.json({ success: false, statusCode: response.statusCode, error: response.body });
+      // Data is successfully parsed as a JSON object
+      console.log("api call successful");
+      console.log(filter);
+      const metaData = Object.values(data)[0];
+      const values = Object.values(data)[1]
+      const stockPrice = Object.values(values).map(obj => obj[filter]).reverse();  //.map(//.map(item => parseFloat(item[info]));
+      const label = Object.keys(values).reverse()
+      console.log(label);
+      console.log(stockPrice);
+      res.json({success: true, stockPrice : stockPrice, label : label, metaData: metaData});
+    }});
+});
 module.exports = router;
